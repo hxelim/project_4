ff33f643c7fd42dd80529ce99cd724e33cbd4425
summer <yuu.hyelim@gmail.com>
2024-03-19 17:34:59 +0900

    240319

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 오늘, 리디의 발견
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v3/search/book?target=publisher",
@@ -5,7 +6,6 @@ $.ajax({
   headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
 })
   .done(function (msg) {
-    console.log(msg);
 
     const divs = $('.recommend .book')
 
@@ -16,6 +16,7 @@ $.ajax({
   });
 
 
+// MD's Pick
 let search;
 search = ['베스트셀러', '소설', '푸바오']
 
@@ -29,7 +30,6 @@ for (let i = 0; i < search.length; i++) {
     headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
   })
     .done(function (msg) {
-      console.log(msg);
 
       const divs = $('.mdPick .book')
 
@@ -39,7 +39,7 @@ for (let i = 0; i < search.length; i++) {
     });
 }
 
-
+// 베스트
 $.ajax({
   method: "GET",
   url: "https://dapi.kakao.com/v3/search/book?target=contents",
@@ -47,7 +47,6 @@ $.ajax({
   headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
 })
   .done(function (msg) {
-    console.log(msg);
 
     const divs = $('.bestlist .book')
 
@@ -60,6 +59,43 @@ $.ajax({
     }
   });
 
+// 새로 나온 책
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "북로망스", size: 50 },
+  headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
+})
+  .done(function (msg) {
+    const divs = $('.new .boxL .bookbox')
+
+    for (let i = 0; i < divs.length; i++) {
+      $(divs).eq(i).find('.bookCover').append("<img src = '" + msg.documents[i].thumbnail + "'>")
+      $(divs).eq(i).find('.bookName').append(msg.documents[i].title);
+      $(divs).eq(i).find('.bookAuthor').append(msg.documents[i].authors);
+      $(divs).eq(i).find('.bookContents').append(msg.documents[i].contents);
+    }
+  });
+
+let boxL = $('.new .boxL .bookbox')
+let idx = 0;
+boxL.eq(0).show().siblings().hide();
+
+$('.new .btn .prev').click(function () {
+  idx -= 1;
+  if (idx < 0) {
+    idx = 6;
+  }
+  $(boxL).eq(idx).fadeIn().siblings().fadeOut();
+})
+$('.new .btn .next').click(function () {
+  idx += 1;
+  if (idx > 6) {
+    idx = 0;
+  }
+  $(boxL).eq(idx).fadeIn().siblings().fadeOut();
+})
+
 
 $.ajax({
   method: "GET",
@@ -68,35 +104,38 @@ $.ajax({
   headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
 })
   .done(function (msg) {
-    console.log(msg);
 
-    const divs = $('.new .bookbox')
+    const divs = $('.new .boxR .bookbox')
 
     for (let i = 0; i < divs.length; i++) {
       $(divs).eq(i).find('.bookCover').append("<img src = '" + msg.documents[i].thumbnail + "'>")
-
       $(divs).eq(i).find('.bookName').append(msg.documents[i].title);
-      $(divs).eq(i).find('.bookAuthor').append(msg.documents[i].authors);
-      $(divs).find('.bookContents').append(msg.documents[i].contents.substring(0, 30));
     }
   });
 
-  $.ajax({
-    method: "GET",
-    url: "https://dapi.kakao.com/v3/search/book?target=contents",
-    data: { query: "영화원작", size: 50 },
-    headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
-  })
-    .done(function (msg) {
-      console.log(msg);
-  
-      const divs = $('.academy .book')
-  
-      for (let i = 0; i < divs.length; i++) {
-        $(divs).eq(i).find('.bookCover').append("<img src = '" + msg.documents[i].thumbnail + "'>")
-  
-        $(divs).eq(i).find('.bookName').append(msg.documents[i].title);
-        $(divs).eq(i).find('.bookName').append("<p>"+msg.documents[i].authors+"</p>");
-        $(divs).find('.bookContents').append(msg.documents[i].contents.substring(0, 30));
-      }
-    });
+
+
+// 아카데미 수상작 & 후보작 원작
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=contents",
+  data: { query: "영화원작", size: 50 },
+  headers: { Authorization: "KakaoAK ec463079ad57ea9cb97f6400249a8755" }
+})
+  .done(function (msg) {
+    const divs = $('.academy .book')
+    const origin = msg.documents;
+    //썸네일이 빈 문자열인것은 제외
+    let data = origin.filter((val)=>{
+        return val.thumbnail != '' && val.contents !='';
+    })
+
+
+    for (let i = 0; i < divs.length; i++) {
+      $(divs).eq(i).find('.bookCover').append("<img src = '" +data[i].thumbnail + "'>")
+
+      $(divs).eq(i).find('.bookName').append(data[i].title);
+      $(divs).eq(i).find('.bookName').append("<p>" + data[i].authors + "</p>");
+      $(divs).find('.bookContents').append(data[i].contents.substring(0, 30));
+    }
+  });
--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,111 @@
-var swiper = new Swiper(".mySwiper", {
+// best
+var swiper1 = new Swiper(".best .mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
 
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".best .swiper-button-next",
+    prevEl: ".best .swiper-button-prev",
   },
-
+  on: {
+    init: function () {
+      $('.best .btn .prev').hide();
+    },
+    slideChange: function(){
+      let idx = this.activeIndex;
+      if (idx != 0){
+        $('.best .btn .prev').fadeIn();        
+      }
+      if(idx == 3){
+        $('.best .btn .next').hide();
+      }
+      if(idx == 0){
+        $('.best .btn .prev').hide();
+      }  
+    if(idx != 3){
+      $('.best .btn .next').fadeIn();
+    }
+  }, 
+} 
 });
+
+
+// new boxR
+var swiper2 = new Swiper(".new .boxR.mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  loop : true,
+  loopAdditionalSlides : 1,
+  allowTouchMove : false,
+
+  navigation: {
+    nextEl: ".new .swiper-button-next",
+    prevEl: ".new .swiper-button-prev",
+  },
+  on: {
+    init: function () {
+      $('.new .btn .prev').hide();
+    },
+    slideChange: function(){
+      let idx = this.activeIndex;
+      if (idx != 0){
+        $('.new .btn .prev').fadeIn();        
+      }
+      // if(idx == 3){
+      //   $('.new .btn .next').hide();
+      // }
+      // if(idx == 0){
+      //   $('.new .btn .prev').hide();
+      // }  
+    if(idx != 3){
+      $('.new .btn .next').fadeIn();
+    }
+  }, 
+} 
+});
+
+
+// academy
+var swiper3 = new Swiper(".academy .mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".academy .swiper-button-next",
+    prevEl: ".academy .swiper-button-prev",
+  },
+  on: {
+    init: function () {
+      $('.academy .btn .prev').hide();
+    },
+    slideChange: function(){
+      let idx = this.activeIndex;
+      if (idx != 0){
+        $('.academy .btn .prev').fadeIn();        
+      }
+      if(idx == 5){
+        $('.academy .btn .next').hide();
+      }
+      if(idx == 0){
+        $('.academy .btn .prev').hide();
+      }  
+    if(idx != 5){
+      $('.academy .btn .next').fadeIn();
+    }
+  }, 
+} 
+});
+
+
+// hit
+var swiper4 = new Swiper(".hit .mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: ".hit .swiper-button-next",
+    prevEl: ".hit .swiper-button-prev",
+  },
+});
+
+
