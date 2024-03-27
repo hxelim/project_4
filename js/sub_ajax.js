4720e046609e1dcca0ec1184bbd825eff911fefc
summer <yuu.hyelim@gmail.com>
2024-03-27 12:14:22 +0900

    240326

--- js/sub_ajax.js
+++ js/sub_ajax.js
@@ -91,4 +91,15 @@ $.ajax({
       $(divs).eq(i).append("<div class = 'bookNamebox'><h3>" + msg.documents[i].title + "</h3></div>");
       $(divs).eq(i).append('<a href="#"></a>')
     }
+
+
+
+    // let list = msg.document[i].title;
+    // console.log(list)
+
+    // $('.sort li').eq(1).find('a').click(function(){
+    //   list.sort();
+    // })
   });
+
+
